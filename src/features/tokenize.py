c0855eb926c4375c8d5d29b13813339d16285e8d
ergarciap <erick.garciap01@gmail.com>
2020-07-06 22:20:31 -0600

    Actualizado

--- src/features/tokenize.py
+++ src/features/tokenize.py
@@ -1,8 +1,9 @@
 import gensim
+from typing import List
 from gensim.models import CoherenceModel
 from src.features.utils import remove_stopwords, sent_to_words, make_bigrams, make_trigrams, lemmatization
 
-def tokenize_classes(data_words: list) -> list:
+def tokenize_classes(data_words) -> List[list]:
 # Construimos modelos de bigrams y trigrams
 # https://radimrehurek.com/gensim/models/phrases.html#gensim.models.phrases.Phrases
     bigram = gensim.models.Phrases(data_words, min_count=5, threshold=100)
